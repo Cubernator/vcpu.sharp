2efe223f949b399e8ad5f3d5495d839e65e7f784
Dennis Heinze <dennisjp.heinze@gmail.com>
2020-04-19 14:18:31 +0200

    add more unit tests

--- test/Test.Vcpu/ProgramTests.cs
+++ test/Test.Vcpu/ProgramTests.cs
@@ -15,10 +15,43 @@ namespace Test.VcpuSharp
 .instructions
 HALT";
 
-            var program = new Program(source);
+            using var program = new Program(source);
 
+            Assert.NotNull(program);
             Assert.Equal(0, program.Data.Size);
             Assert.Equal(4, program.Instructions.Size);
+        }
+
+        [Fact]
+        public void MinimalProgramRuns()
+        {
+            var source = @"
+.data
+.instructions
+HALT";
+
+            using var program = new Program(source);
+            using var processor = new Processor();
+            using var memory = new PlainMemory(4);
+
+            Assert.NotNull(program);
+            Assert.NotNull(processor);
+            Assert.NotNull(memory);
+
+            processor.Run(program, memory);
+
+            Assert.Equal(0, processor.State);
+        }
+
+        [Fact]
+        public void InvalidProgramFailsToAssemble()
+        {
+            var source = "INVALIDPROGRAM";
+
+            var exception = Assert.Throws<VcpuException>(() =>
+            {
+                using var program = new Program(source);
+            });
         }
     }
 }
